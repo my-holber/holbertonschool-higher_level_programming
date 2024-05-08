6f153d11f9acf2501e203b7da1151640af43c56f
my-holber <8762@holbertonstudents.com>
2024-05-08 13:19:53 +0200

    test

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end="")
+for letter_code in range(ord('a'), ord('z')+1):
+    letter = chr(letter_code)
+    print(letter, end="")
