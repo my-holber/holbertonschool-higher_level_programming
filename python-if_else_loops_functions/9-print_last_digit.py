f28be4244faf7fd7149336709a804646236e0fbc
my-holber <8762@holbertonstudents.com>
2024-05-09 18:07:05 +0200

    abs

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 def print_last_digit(number):
+    if number < 0:
+        number = abs(number)
     _result = number % 10
     print("{:d}".format(_result), end=(""))
     return(_result)
