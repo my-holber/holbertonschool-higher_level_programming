672ea18a57fb75065961af99cb2ca91705e42b21
my-holber <8762@holbertonstudents.com>
2024-05-13 17:14:48 +0200

    fix 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     my_list2 = my_list[:]
     if idx < 0:
         return my_list2
-    if idx > len(my_list):
+    if idx > len(my_list) -1:
         return my_list2
     else:
         my_list2[idx] = element
