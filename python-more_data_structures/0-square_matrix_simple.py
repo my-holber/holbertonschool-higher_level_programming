910475b6db4bdf84ab459f1bc702c5b8708fd8dc
my-holber <8762@holbertonstudents.com>
2024-05-14 19:49:19 +0200

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for row in matrix:        # Внешний цикл проходит по каждой строке матрицы
+    for row in matrix:
         new_row = []
-        for num in row:       # Внутренний цикл проходит по каждому элементу внутри строки
-            new_row.append(num ** 2)  # Квадрат каждого элемента добавляется в new_row
-        new_matrix.append(new_row)    # new_row добавляется в new_matrix
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
     return new_matrix
