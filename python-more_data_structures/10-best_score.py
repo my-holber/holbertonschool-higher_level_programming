70a3bf8d2c7e48957901645ca959fb6d6e1abf31
my-holber <8762@holbertonstudents.com>
2024-05-15 15:14:42 +0200

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    else:
-        _dict = max(a_dictionary)
-        if not _dict:
-            return None
-        return _dict
+    max_value = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == max_value:
+            return key
