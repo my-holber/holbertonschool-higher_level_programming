d1526f923b3052cba565a3c38df19701c3f7a7c4
my-holber <8762@holbertonstudents.com>
2024-05-15 15:04:07 +0200

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        return max(a_dictionary)
+        _dict = max(a_dictionary)
+        if not _dict:
+            return None
+        return _dict
