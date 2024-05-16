5fdba6a97ac66c56d4128cae4ccd9fd8a1335eb6
my-holber <8762@holbertonstudents.com>
2024-05-16 14:40:14 +0200

    3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except (ZeroDivisionError, ValueError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
