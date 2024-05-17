6353ad5646cee0cead59737f2ba4acecbc32f597
my-holber <8762@holbertonstudents.com>
2024-05-17 21:24:11 +0200

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -17,14 +17,20 @@ class Square:
         self.size = size
 
     def area(self):
+        """Get the current square area."""
         return (self.__size ** 2)
 
     @property
     def size(self):
+    """Get the size of square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter of the value size.
+            Args:
+                value (int): Value corresponding to the size.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -32,6 +38,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """Prints in stdout the square with the character #."""
         if self.__size < 0:
             print("")
         else:
