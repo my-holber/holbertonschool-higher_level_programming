ac5aaae54f5b922e0b32bb748f5cc84b10f391b3
my-holber <8762@holbertonstudents.com>
2024-05-17 14:45:28 +0200

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
+        self.__size = size
