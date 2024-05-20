bc0bb0bb6a70ac4a97f47264f62d89fec3d0aeda
my-holber <8762@holbertonstudents.com>
2024-05-20 07:29:58 -0700

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,24 +5,15 @@ qui décrit le fichier .py"""
 
 def add_integer(a, b=98):
     """Adds two integers or floats,
-       casted to integers first if they are floats.
-
-    Args:
-        a: The first number, must be an integer or float.
-        b: The second number,
-        must be an integer or float, default is 98.
-
-    Returns:
-        The integer sum of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+        casted to integers first if they are float
     """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(a)
+    if not isinstance(a, (int)):
+        a = int(a)
+    if not isinstance(b, (int)):
+        b = int(a)
     return a + b
