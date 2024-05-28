e873ffed1cf12281e4d0f70159305f116b2f0c29
my-holber <159010114+my-holber@users.noreply.github.com>
2024-05-28 16:07:46 +0200

    .\2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -14,5 +14,5 @@ def append_write(filename="", text=""):
 
     """
 
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
