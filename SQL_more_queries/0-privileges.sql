836356222dc8b8d9b6a11bbdfd22c3ea22c55982
my-holber <8762@holbertonstudents.com>
2024-06-20 14:51:00 +0200

    add file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---a script that lists all MySQL user privileges
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- a script that lists all MySQL user privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2@'localhost';
