73bfd50a7780fdfaeae5d91c7f6788885af1dbe9
my-holber <8762@holbertonstudents.com>
2024-06-20 16:26:32 +0200

    4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
---скрипт, который создает таблицу id_not_null на вашем сервере MySQL.
+-- script that creates the table id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS id_not_null(
 	id INT DEFAULT 1,
 	name VARCHAR(256)
-)ENGINE=INNODB;
+)
