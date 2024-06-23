d07cbdd39d0fbe142deb35ac72b67faace475e02
my-holber <8762@holbertonstudents.com>
2024-06-24 00:28:59 +0200

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,17 @@
-#!/usr/bin/python3
-"""
-    Script that lists all states from the database.
-"""
-import MySQLdb
 import sys
-
-
-def list_states_with_n(username, password, database):
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database)
-    cursor = db.cursor()
-
-    # Execute the query
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all rows
-    states = cursor.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
+import MySQLdb
+k  # !/usr/bin/python3
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    list_states_with_n(username, password, database)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
