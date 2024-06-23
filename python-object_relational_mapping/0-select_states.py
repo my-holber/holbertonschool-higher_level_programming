4cc05f43d58197e817c7094e6d26802fd2b65495
my-holber <8762@holbertonstudents.com>
2024-06-23 23:54:29 +0200

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,37 +6,12 @@ import MySQLdb
 import sys
 
 
-def list_states(username, password, database):
-    # Connect to MySQL database
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database)
-    cursor = db.cursor()
-
-    # Execute the query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows
-    states = cursor.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py username password database")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    list_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
