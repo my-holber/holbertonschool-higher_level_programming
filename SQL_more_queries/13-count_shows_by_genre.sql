9f757b5930a5c9c7ec38785d0b15afe5c562b0cb
my-holber <8762@holbertonstudents.com>
2024-06-23 15:38:34 +0200

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,14 +1,11 @@
 -- Number of shows by genre
 SELECT
-    tv_genres.name,
+    tv_genres.name AS genre,
     COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM
     tv_genres
-LEFT JOIN
+        LEFT JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY
-    tv_genres.name
-HAVING
-    number_of_shows > 0
-ORDER BY
-    number_of_shows DESC;
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
