6065c3b74924565ce09ab34fd3fa1689e5cbab44
my-holber <8762@holbertonstudents.com>
2024-06-24 00:39:45 +0200

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Script that lists all states from the database.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = db.cursor()
+    state_name = sys.argv[4]
+    cur.execute(
+        f"SELECT * FROM states WHERE name = '{state_name}' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
