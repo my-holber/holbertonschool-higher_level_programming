7b4f7e844b79d0833579f736289bd2ec172302a1
my-holber <8762@holbertonstudents.com>
2024-06-24 00:46:40 +0200

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,11 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
     cur = db.cursor()
     state_name = sys.argv[4]
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
