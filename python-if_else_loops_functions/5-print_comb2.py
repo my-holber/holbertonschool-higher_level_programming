be8e2468ba1d12c1bb33e9f34f0f4bf92b70ea7f
my-holber <8762@holbertonstudents.com>
2024-09-03 22:58:41 +0300

    add

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-for i in range(99):
+for i in range(100):
     if i < 99:
-        print(f"{i:02}", end=', ')
-    print(f'{i:02}', end='')
+        print(f'{i:02}', end=', ')
+    else:print(f'{i:02}')
