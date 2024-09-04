89f3b1b5bc9f267ba95eb7957ad2aca8820918f6
my-holber <8762@holbertonstudents.com>
2024-09-04 11:34:12 +0300

    add

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numberfirst in range(0, 9):
     for numbersecond in range(numberfirst + 1, 10):
-        print(f'{numberfirst}{numbersecond}', end=', ')
+        print('{}{}'.format(numberfirst, numbersecond), end=', ')
