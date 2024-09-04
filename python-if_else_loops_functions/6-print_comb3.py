710f3af3db14f3d9c310db3f54daca73411f0aad
my-holber <8762@holbertonstudents.com>
2024-09-04 11:51:19 +0300

    add

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for numberfirst in range(0, 9):
     for numbersecond in range(numberfirst + 1, 10):
-        print('{}{}'.format(numberfirst, numbersecond), end=', ')
+        if numberfirst == 8 and numbersecond == 9:
+            print('{}{}'.format(numberfirst, numbersecond))
+        else:
+            print('{}{}'.format(numberfirst, numbersecond), end=', ')
