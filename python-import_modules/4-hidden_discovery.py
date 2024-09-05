6d8f326289e319fb831181d6a513bab86f9794e7
my-holber <8762@holbertonstudents.com>
2024-09-05 15:45:33 +0300

    add

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from hidden_4 import *
-
-    names = [name for name in dir() if not name.startswith("__")]
-    names.sort()
-    for name in names:
-        print(name)
+    names = dir()
+    for i in range(0, len(names)):
+        if (names[i][:2] != "__"):
+            print("{:s}".format(names[i]))
