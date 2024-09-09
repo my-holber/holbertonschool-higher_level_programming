b49996992567b25b3fd87b2068751bc06763435d
my-holber <8762@holbertonstudents.com>
2024-09-09 16:59:13 +0300

    add

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,3 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{}".format(element) for element in row))
+        print(" ".join("{:d}".format(element) for element in row))
