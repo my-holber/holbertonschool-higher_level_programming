02cdfd64114b7f5e9d08e98684288a2582dd2fe1
my-holber <8762@holbertonstudents.com>
2024-09-15 19:28:12 +0300

    add

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,33 +1,5 @@
 #!/usr/bin/python3
 """
- This function that prints a text with 2 new lines
- after each of these characters: ., ? and :
-"""
-
-
-def text_indentation(text):
-    """
-    Return: the result of the text
-    """
-
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    skip_next = False
-    for idx, i in enumerate(text):
-        if skip_next and i == ' ':
-            skip_next = False
-            continue
-        elif skip_next:
-            skip_next = False  # Do not skip valid characters like letters
-        if i in [".", ":", "?", "!"]:
-            print(i, end="\n")
-            print()
-            skip_next = True
-        else:
-            print(i, end="")
-#!/usr/bin/python3
-"""
     Function file for text_indentation.
 """
 
