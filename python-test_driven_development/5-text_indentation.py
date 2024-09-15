1dcc17c2dba840ba0b1266616bb6dc18bd009a54
my-holber <8762@holbertonstudents.com>
2024-09-15 19:16:36 +0300

    add

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,10 @@ def text_indentation(text):
     """
     Return: the result of the text
     """
-
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-
-    result = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
-    print(result)
+    result = text.replace('.', '.\n\n').replace
+    ('?', '?\n\n').replace(':', ':\n\n')
+    lines = result.split('\n')
+    cleaned_lines = [line.strip() for line in lines]
+    print('\n'.join(cleaned_lines))
