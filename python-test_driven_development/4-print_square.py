92562847f4fb3a4c31fbc249b3a9d4cc7385c4e4
my-holber <8762@holbertonstudents.com>
2024-09-15 18:32:26 +0300

    add

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,25 +1,14 @@
+#!/usr/bin/python3
+
 def print_square(size):
+    """
+    Return: print # with number size
+    """
+
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    
+
     for _ in range(size):
         print('#' * size)
-
-
-#!/usr/bin/python3
-
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")        
