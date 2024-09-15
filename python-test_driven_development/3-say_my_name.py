bfc179923776357a8e36e1e9adb213ae96eedcec
my-holber <8762@holbertonstudents.com>
2024-09-15 18:18:06 +0300

    add

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,15 @@
+#!/usr/bin/python3
+
 def say_my_name(first_name, last_name=""):
+    """
+    prints the first name and last name.
+    first_name and last_name must be strings.
+    raise a TypeError if first_name or last_name is not a string.
+    print message with first_name and last_name.
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
-    if not isinstance (last_name, str):
+    if not isinstance(last_name, str):
         raise TabError('last_name must be a string')
 
     print(f"My name is {first_name} {last_name}")
-
-
-#!/usr/bin/python3
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)    
