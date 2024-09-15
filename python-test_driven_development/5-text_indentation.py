d36b9de3003db7ff4d580b802bb0d2f172f75e14
my-holber <8762@holbertonstudents.com>
2024-09-15 19:25:15 +0300

    add

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -26,3 +26,29 @@ def text_indentation(text):
             skip_next = True
         else:
             print(i, end="")
+#!/usr/bin/python3
+"""
+    Function file for text_indentation.
+"""
+
+
+def text_indentation(text):
+    """
+        Prints a text with 2 new lines after each characters
+        `.`, `?`, `:`
+        Arguments:
+            text (str): Text to formated.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
