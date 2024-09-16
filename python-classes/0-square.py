5de081239fbb8625b535d3caa69e7e77070cf5b2
my-holber <8762@holbertonstudents.com>
2024-09-16 16:05:08 +0300

    add

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This is class Square that defines a square
+"""
+
 class Square():
+    """
+    Return: a empty class square
+    """
     def __init__(self):
         pass
