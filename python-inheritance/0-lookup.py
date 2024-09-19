f3a18a146fe3087f1411799a9cdfecb582d55c1c
my-holber <8762@holbertonstudents.com>
2024-09-19 14:57:45 +0300

    add

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+'''
+This script defines a function to list all
+attributes and methods of an object.
+'''
+
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings representing the names of the object's attributes and
+        methods.
+        Use dir() to get a list of all attributes and methods of the object
+    """
     return dir(obj)
