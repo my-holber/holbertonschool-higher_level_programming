b7c9c675fce9798535fe7f83a617b04c9a7a11fe
my-holber <8762@holbertonstudents.com>
2024-09-21 20:27:11 +0300

    add

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
                 name (str): Name of the value.
                 value (int): Value.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('{} must be an integer'.format)
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
