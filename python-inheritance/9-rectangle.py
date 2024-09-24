f4946fd85c1bccda8e2aef29cdd873982fd0bba3
my-holber <8762@holbertonstudents.com>
2024-09-24 15:09:01 +0300

    add

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -6,10 +6,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle class inherits from BaseGeomtry. """
+    """ Rectangle class inherits from BaseGeometry. """
     def __init__(self, width, height):
         """
-            Intialization of Rectangle.
+            Initialization of Rectangle.
             Arguments:
                 width (int): Width of the rectangle.
                 height (int): Height of the rectangle.
@@ -18,3 +18,18 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+    
+    def area(self):
+        """ Returns the area of the rectangle. """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Returns the string representation of the rectangle. """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+# Example usage
+r = Rectangle(3, 5)
+
+print(r)         # Output: [Rectangle] 3/5
+print(r.area())  # Output: 15
