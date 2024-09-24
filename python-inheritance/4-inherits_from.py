9613a3989f19cf709db82dd92dcddc8ed2f9453b
my-holber <8762@holbertonstudents.com>
2024-09-24 17:31:57 +0300

    add

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,4 +14,6 @@ def inherits_from(obj, a_class):
         Return:
             (bool) Resultat.
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
