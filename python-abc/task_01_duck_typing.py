a35cb9152b438712c4301a50dd4e726ae0eb0243
luffy <luffy@luffy.luffy>
2024-09-27 11:47:58 -0800

    add

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,72 +1,42 @@
-#!/usr/bin/env python3
-"""Shapes, Interfaces, and Duck Typing"""
-from abc import ABC, abstractmethod
+#!/usr/bin/python3
+from abc import ABC
+from abc import abstractmethod
 from math import pi
 
 
 class Shape(ABC):
-    """
-    Abstract base class for geometric shapes
-    """
     @abstractmethod
     def area(self):
-        """
-        Abstract method to calculate area
-        """
         pass
 
     @abstractmethod
     def perimeter(self):
-        """
-        Abstract method to calculate perimeter
-        """
         pass
 
 
 class Circle(Shape):
-    """
-    A subclass of Shape that represents a circle
-    """
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        """
-        Returns the area of the circle
-        """
         return pi * self.radius ** 2
 
     def perimeter(self):
-        """
-        Returns the perimeter (circumference) of the circle
-        """
-        return 2 * pi * abs(self.radius)
+        return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
-    """
-    A subclass of Shape that represents a rectangle
-    """
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
-        """
-        Returns the area of the rectangle
-        """
         return self.width * self.height
 
     def perimeter(self):
-        """
-        Returns the perimeter of the rectangle
-        """
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """
-    Prints the area and perimeter of a shape
-    """
-    print("Area:", shape.area())
-    print("Perimeter:", shape.perimeter())
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
