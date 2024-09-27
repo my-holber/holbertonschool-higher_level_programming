abafe9aa71dc7cb91102feabdcece6299653f0a8
luffy <luffy@luffy.luffy>
2024-09-27 11:46:43 -0800

    add

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
 
 class VerboseList(list):
-    """
-    A subclass of the built-in list that provides verbose output
-    for certain operations (append, extend, remove, pop).
-    """
     def append(self, item):
-        """
-        Adds an item to the list and prints a message
-        """
-        print("Added [{}] to the list.".format(item))
         super().append(item)
+        print(f"Added [{item}] to the list.")
 
     def extend(self, item):
-        """
-        Extends the list with multiple items and prints a message
-        """
-        print("Extended the list with [{}] items.".format(len(item)))
         super().extend(item)
+        print(f"Extended the list with [{len(item)}] items.")
 
     def remove(self, item):
-        """
-        Removes an item from the list and prints a message
-        """
-        print("Removed [{}] from the list.".format(item))
         super().remove(item)
+        print(f"Removed [{item}] from the list.")
 
-    def pop(self, index=-1):
-        """
-        Pops an item from the list and prints a message
-        """
-        if self:
-            pop_item = self[index]
-            print("Popped [{}] from the list.".format(pop_item))
-            return super().pop(index)
+    def pop(self, item=-1):
+        if item == -1:
+            res = super().pop()
         else:
-            raise ValueError("Cannot pop from an empty list.")
+            res = super().pop(item)
+        print(f"Removed [{res}] from the list.")
