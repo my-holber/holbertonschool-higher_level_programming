41806b6b23dd06121260fb3ed9ee5fee094a1d82
luffy <luffy@luffy.luffy>
2024-09-28 09:51:07 -0800

    add

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    function to write a text file
+"""
+
+
 def write_file(filename="", text=""):
     '''Use 'with' to open the file in write mode'''
     with open(filename, 'w', encoding='UTF-8') as file:
