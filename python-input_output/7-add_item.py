b581882339eb034fa404ecc74df977e345c7937b
luffy <luffy@luffy.luffy>
2024-10-01 17:20:26 +0300

    add

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""7. Load, add, save"""
+
+
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+'''Загружаем существующие элементы или создаем пустой список'''
+if os.path.exists('add_item'):
+    items = load_from_json_file(filename)
+else:
+    items = []
+arguments = sys.argv[1:]
+items.extend(arguments)
+save_to_json_file(items, filename)
