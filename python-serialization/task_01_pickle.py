da323be5ff955c5d115f556f9d08efca61caa510
luffy <luffy@luffy.luffy>
2024-10-04 15:52:16 +0300

    add

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
-"""1. Pickling Custom Classes"""
 
 import pickle
 
 
 class CustomObject:
-    def __init__(self, name, age, is_student):
-        """
+    """
         Initializes the CustomObject with name, age, and student status.
-        """
+    """
+
+    def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
         """
-        Displays the attributes of the object.
+            Displays the attributes of the object.
         """
-        print("Name: {}\nAge: {}\nIs Student: {}\n"
-              .format(self.name, self.age, self.is_student))
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """
-        Serializes the object and saves it to a file using pickle.
-        """
+        '''Serialize the instance itself'''
         with open(filename, "wb") as file:
             pickle.dump(self, file)
 
     @classmethod
     def deserialize(cls, filename):
-        """
-        Deserializes the object from a file using pickle.
-        Returns None if the file is not found.
-        """
-        try:
-            with open(filename, "rb") as file:
-                return pickle.load(file)
-        except Exception:
-            return None
+        # Deserialize and return an instance of CustomObject
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
