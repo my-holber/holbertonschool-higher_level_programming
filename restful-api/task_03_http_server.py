6c38638846ed19a699edc045a9569f540116ba83
luffy <luffy@luffy.luffy>
2024-10-13 15:21:08 +0300

    add

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,48 @@
+import http.server
+import json
+import socket
+from http.server import HTTPServer
+
+
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
+
+
+print(get_local_ip())
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            print(self.path)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.wfile.write(b"Hello, this is a simple API")
+
+        elif self.path == '/data':
+            data = {"name": "John",
+                    "age": 30,
+                    "city": "New York"}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found')
+
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd server on port {}'.format(port))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
