93b17544d4aeb8041f1a53fa07ef3306135a8284
luffy <luffy@luffy.luffy>
2024-10-13 15:30:18 +0300

    add

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,23 +3,32 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class MyHandler(BaseHTTPRequestHandler):
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
+
         if self.path == '/':
-            print(self.path)
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
-            self.wfile.write(b"Hello, this is a simple API")
-
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
-            data = {"name": "John",
-                    "age": 30,
-                    "city": "New York"}
+
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
-
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
@@ -27,12 +36,12 @@ class MyHandler(BaseHTTPRequestHandler):
             self.wfile.write(b'Endpoint not found')
 
 
-def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
-    server_address = ("", port)
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print('Starting httpd server on port {}'.format(port))
+    print("Serving on port {}...".format(port))
     httpd.serve_forever()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
