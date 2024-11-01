60baab640ebd41c881eda3f0ad744709e5b2190b
my-holber <8762@holbertonstudents.com>
2024-11-01 15:29:03 +0300

    add

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score
-SELECT second_table COUNT(*) AS number
+SELECT score COUNT(*) AS number
 FROM second_table
 GROUP BY score;
 ORDER BY number DESC;
