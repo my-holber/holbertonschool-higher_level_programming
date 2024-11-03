ee8bbbc5f029c0935e08e352d4d88b9f571c067c
my-holber <8762@holbertonstudents.com>
2024-11-03 22:53:19 +0300

    add

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
 """
-    Script that lists all states from the database.
+    Script that lists all states from the database where the name matches the given argument.
+    The script is safe from SQL injections.
 """
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
+    # Подключение к базе данных
     conn = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
-        )
-
+    )
     name = sys.argv[4]
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                        ORDER BY states.id".format(name))
+    
+    # Подготовка запроса с защитой от SQL-инъекций
+    query = (
+        "SELECT states.id, states.name "
+        "FROM states "
+        "WHERE states.name LIKE BINARY %s "
+        "ORDER BY states.id ASC"
+    )
+    
+    # Передача параметра как кортежа из одного элемента
+    cursor.execute(query, (name,))
+
+    # Вывод результата
     rows = cursor.fetchall()
-    for i in rows:
-        print(i)
+    for row in rows:
+        print(row)
+    
+    # Закрытие курсора и подключения
     cursor.close()
     conn.close()
+
