ed33291e6b460ef38bcd4ec12c3aad2f176a91ef
my-holber <8762@holbertonstudents.com>
2024-11-03 19:43:40 +0300

    add

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Script that lists all states from the database.
+"""
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host = 'localhost',
+                       user = sys.argv[1],
+                       passwd = sys.argv[2],
+                       db = sys.argv[3],
+                       port = 3306
+                       )
+if __name__ == '__main__':
+    cursor = conn.cursor()
+    cursor.execute('SELECT states.id, states.name FROM states')
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    cursor.close()
+    conn.close()
